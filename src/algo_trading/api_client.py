f831193f399bb3322aaace0d81b30c49e9c9daab
Aymane <aymanecheikh2002@gmail.com>
2025-11-16 03:53:32 +0000

    boilerplate

--- src/algo_trading/api_client.py
+++ src/algo_trading/api_client.py
@@ -1,9 +1,55 @@
-import requests, time, os
 from pprint import pprint
+import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning # type: ignore
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+import numpy as np
 
 
+class MarketSession:
+    def __init__(self):
+        self.BASE_URL = 'https://localhost:5000/v1/api'
+        self.ISERVER_BASE = f'{self.BASE_URL}/iserver'
+        self.session = requests.Session()
+        self.session_init # Defined below
+
+    @property
+    def session_init(self) -> dict:
+        self.session.verify = False
+        session_init = self.session.post(
+            url=f'{self.ISERVER_BASE}/auth/ssodh/init',
+            json={'publish': True, 'compete': True}
+        )
+        session_init_details = session_init.json()
+        assert session_init.status_code == 200
+        assert session_init_details['authenticated'] == True
+        assert session_init_details['connected'] == True
+        return session_init_details
+
+    @property
+    def account(self) -> str:
+        account_info = self.session.get(
+            url=f'{self.BASE_URL}/portfolio/accounts'
+        )
+        assert account_info.status_code == 200
+        return account_info.json()[0]['accountId']
+
+class AssetEcosystem:
+    def __init__(self):
+        self.s = MarketSession()
+        self.scope: dict = self.s.session.get(
+            url=f'{self.s.ISERVER_BASE}/secdef/search',
+            params={'symbol': 'BTC', 'name': True}
+        ).json()
+
+
+if __name__ == '__main__':
+    ae = AssetEcosystem()
+    details = ae.scope
+    company_names = np.array([detail['companyName'] for detail in details])
+    print(np.nonzero(company_names == None))
+
+
+'''
 BASE_URL = 'https://localhost:5000/v1/api'
 session = requests.Session()
 session.verify = False
@@ -21,7 +67,7 @@ bitcoin_id = session.get(
     params={'symbol': 'BTC', 'name': True}
 ).json()[0]
 
-'''
+
 market_order = session.post(
     url=f'{BASE_URL}/iserver/account/{account}/orders',
     headers={'Content-Type': 'application/json'},
@@ -41,7 +87,7 @@ market_order = session.post(
 )
 
 pprint(market_order.json())
-'''
+
 
 order_status = session.get(f'{BASE_URL}/iserver/account/order/status/474467663')
 pprint(order_status.json())
@@ -56,11 +102,12 @@ positions = (
 print(f'\nFETCHING ACCOUNT: {account}\n')
 
 
-'''
+
 print(f'---- FETCHING PORTFOLIO ----\n')
 pprint(portfolio)
-'''
+
 
 print(f'---- FETCHING POSITIONS ----\n')
 print(f'Positions endpoint response code: {positions.status_code}')
 pprint(positions.json())
+'''
