ff51b2ec71fdf91f3a6e4d1a59541b85cd2a90ef
Aymane <aymanecheikh2002@gmail.com>
2025-11-25 23:11:00 +0000

    added enum constants for fundamental data and built out fundamental functions for each endpoint. Looking to test each and turn into class potentially

--- src/algo_trading/constants.py
+++ src/algo_trading/constants.py
@@ -1,8 +1,20 @@
+from enum import Enum
 from typing import Final
 import logging, logger
 
 logging.info('Initializing constants')
+
 TOP_N_SCANNER_RESULTS: Final = 20
+
 HOST: Final = "127.0.0.1"
 PORT: Final = 4002
 CLIENT_ID: Final = 1
+
+
+class Fundamentals(Enum):
+    FINANCIAL_SUMMARY = "ReportsFinSummary"
+    COMPANY_OWNERSHIP = "ReportsOwnership"
+    COMPANY_FINANCIAL_OVERVIEW = "ReportSnapshot"
+    FINANCIAL_STATEMENTS = "ReportsFinStatements"
+    ANALYST_ESTIMATES = "RESC"
+    COMPANY_CALENDAR = "CalendarReport"
--- src/algo_trading/data_gateway/fundamental_data.py
+++ src/algo_trading/data_gateway/fundamental_data.py
@@ -5,7 +5,7 @@ logging.info('Importing dependencies for fundamental data fetching')
 from bs4 import BeautifulSoup as bs
 from ib_async import IB, Stock
 
-from constants import HOST, PORT, CLIENT_ID
+from constants import Fundamentals, HOST, PORT, CLIENT_ID
 
 
 logging.info('Connecting to broker')
@@ -13,12 +13,39 @@ ib = IB()
 ib.connect(HOST, PORT, clientId=CLIENT_ID)
 
 
-def fundamental_data(stock: str):
+def fetch_stock_details(stock: str):
     logging.info('Fetching stock object')
-    stock = Stock(stock, 'SMART', 'USD')
-    logging.info('Requesting fundamental data on stock')
+    logging.warning(
+        'Shouldn\'t you be setting \'SMART\' and \'USD\' as '
+        'constants?'
+    )
+    return Stock(stock, 'SMART', 'USD')
+
+def financial_summary(stock: str):
+    logging.info('Requesting financial summary on stock')
     fundamentals = ib.reqFundamentalData(
-        stock, 'ReportSnapshot'
+        fetch_stock_details(stock),
+        Fundamentals.FINANCIAL_SUMMARY.value
+    )
+    logging.info('Parsing xml response')
+    content = bs(fundamentals, 'xml')
+    return content
+
+def company_ownership(stock: str):
+    logging.info('Requesting ownership on stock')
+    fundamentals = ib.reqFundamentalData(
+        fetch_stock_details(stock),
+        Fundamentals.COMPANY_OWNERSHIP.value
+    )
+    logging.info('Parsing xml response')
+    content = bs(fundamentals, 'xml')
+    return content
+
+def company_financial_overview(stock: str):
+    logging.info('Requesting financial overview on stock')
+    fundamentals = ib.reqFundamentalData(
+        fetch_stock_details(stock),
+        Fundamentals.COMPANY_FINANCIAL_OVERVIEW.value
     )
     logging.info('Parsing xml response')
     content = bs(fundamentals, 'xml')
@@ -33,6 +60,14 @@ def fundamental_data(stock: str):
         print(ratio['FieldName'])
         print(ratio.text)
 
+def company_financial_statements(stock: str):
+    logging.info('Requesting financial statements on stock')
+    fundamentals = ib.reqFundamentalData(
+        fetch_stock_details(stock),
+        Fundamentals.FINANCIAL_STATEMENTS.value
+    )
+    return fundamentals
+
 
 if __name__ == '__main__':
-    fundamental_data('ALB')
+    print(company_financial_statements('AAPL'))
